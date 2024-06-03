81d53e2cef726103721e19f5bbffdb3621e2b97b
a-dimitri <antoni.dimitriadis@gmail.com>
2024-06-03 14:51:16 +1000

    Cleaned up command line usage

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -3,12 +3,15 @@
 #include "trie.hpp"
 #include "union-find.hpp"
 #include <iostream>
+#include <string>
 #include <cassert>
 
 using namespace std;
 
 // Sub-routine for testing binary-search
 void _test_binary_search() {
+    cout << "--------------------------------------" << endl;
+    cout << "Testing binary search..." << endl;
     vector<int> arr = {0,1,2,3,4,6,7,8,9,10};
     assert(arr[search(arr, 0)] == 0);
     assert(arr[search(arr, 7)] == 7);
@@ -22,11 +25,14 @@ void _test_binary_search() {
     assert(arr[upper_bound(arr, 5)] == 6);
     assert(arr[lower_bound(arr, 8)] == 8);
     assert(arr[upper_bound(arr, 8)] == 8);
+    cout << "All binary search tests passed" << endl;
+    cout << "--------------------------------------" << endl;
 }
 
 // Sub-routine for testing various graph algorithms
 void _test_graph_algos() {
-
+    cout << "--------------------------------------" << endl;
+    cout << "Testing graph algorithms..." << endl;
     // Test BFS
     vector<vector<int>> edges = {{0,1},{1,2},{2,3},{0,2},{1,4}};
     auto G = buildGraph(5, edges);
@@ -59,10 +65,14 @@ void _test_graph_algos() {
     vector<vector<int>> E = {{0,1,1},{1,2,2},{2,3,5},{0,2,3},{0,3,4}};
     auto T = kruskals(4, E);
     assert( ( T == vector<vector<int>> {{0,1,1}, {1,2,2}, {0,3,4}}) );
+    cout << "All graph algorithm tests passed" << endl;
+    cout << "--------------------------------------" << endl;
 }
 
 // Sub-routine for testing tries
 void _test_trie() {
+    cout << "--------------------------------------" << endl;
+    cout << "Testing trie..." << endl;
     TrieLC T = TrieLC();
     T.insert("apple");
     assert(T.search("apple"));
@@ -78,10 +88,14 @@ void _test_trie() {
     assert(T2.startsWith("4pp"));
     T2.insert("4pp");
     assert(T2.search("4pp"));
+    cout << "All trie tests passed" << endl;
+    cout << "--------------------------------------" << endl;
 }
 
 
 void _test_union_find() {
+    cout << "--------------------------------------" << endl;
+    cout << "Testing union find..." << endl;
     UnionFind UF = UnionFind(6);
     assert(UF.Find(4) == 4);
     UF.Union(0,1);
@@ -90,35 +104,44 @@ void _test_union_find() {
     assert(UF.Find(2) == UF.Find(3));
     UF.Union(1,2);
     assert(UF.Find(0) == UF.Find(3));
+    cout << "All union find tests passed" << endl;
+    cout << "--------------------------------------" << endl;
 }
+
+void _print_usage() {
+    cout << "To run all tests, use [-a | --all]" << endl;
+    cout << "To see a full list of valid commands, use [-h | --help]" << endl;
+};
+
+void _print_help() {
+    cout << "Here is a list of all valid commands to run tests for individual modules: " << endl;
+    cout << "    [-a | --all]                 Tests all modules" << endl;
+    cout << "    [-bs | --binary_search]      Tests the binary search module" << endl;
+    cout << "    [-t | --trie]                Tests the trie module" << endl;
+    cout << "    [-uf | --union_find]         Tests the union find module" << endl;
+    cout << "    [-g | --graph_algorithms]    Tests the graph algorithms module" << endl;
+};
 
 int main(int argc, char const *argv[])
 {   
-    cout << "--------------------------------------" << endl;
-    cout << "--------------------------------------" << endl;
-    cout << "Testing binary search..." << endl;
-    _test_binary_search();
-    cout << "All binary search tests passed" << endl;
-    cout << "--------------------------------------" << endl;
-
-    cout << "--------------------------------------" << endl;
-    cout << "Testing trie..." << endl;
-    _test_trie();
-    cout << "All trie tests passed" << endl;
-    cout << "--------------------------------------" << endl;
-
-    cout << "--------------------------------------" << endl;
-    cout << "Testing union find..." << endl;
-    _test_union_find();
-    cout << "All union find tests passed" << endl;
-    cout << "--------------------------------------" << endl;
-
-    cout << "--------------------------------------" << endl;
-    cout << "Testing graph algorithms..." << endl;
-    _test_graph_algos();
-    cout << "All graph algorithm tests passed" << endl;
-    cout << "--------------------------------------" << endl;
-    cout << "--------------------------------------" << endl;
-
+    using namespace std::literals;
+    bool bs = false, t = false, uf = false, g = false;
+    if ( argc == 1 ) _print_usage();
+    else if ( argv[1] == "-h"sv || argv[1] == "--help"sv ) _print_help();
+    else if ( argv[1] == "-a"sv || argv[1] == "--all"sv) bs = t = uf = g = true;
+    else { 
+        for ( int x = 1; x < argc; ++x ) {
+            if ( argv[x] == "-bs"sv | argv[x] == "--binary_search"sv ) bs = true;
+            else if ( argv[x] == "-t"sv | argv[x] == "--trie"sv ) t = true;
+            else if ( argv[x] == "-uf"sv | argv[x] == "--union_find"sv ) uf = true;
+            else if ( argv[x] == "-g"sv | argv[x] == "--graph_algorithms"sv ) g = true;
+        }
+    }
+    if ( bs || t || uf || g ) cout << "--------------------------------------" << endl;
+    if ( bs ) _test_binary_search();
+    if ( t ) _test_trie();
+    if ( uf ) _test_union_find();
+    if ( g ) _test_graph_algos();
+    if ( bs || t || uf || g ) cout << "--------------------------------------" << endl;
     return 0;
 }
